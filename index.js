878e2e59befc603375da71405341a5b984f8ab1b
Tareq1203088 <trahmanbpdb19@gmail.com>
2025-05-18 13:02:35 +0600

    auth integrated,user  post, get done

--- index.js
+++ index.js
@@ -28,8 +28,10 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
       //crate the db
-    const coffeeCollection = client.db('coffeeDB').collection('coffees')
+    const coffeeCollection = client.db('coffeeDB').collection('coffees');
+    const usersCollection = client.db("coffeeDB").collection('users')
         //show data to server:/3000/coffees (json data)
+              //coffee related data
     app.get("/coffees", async(req,res)=>{
       const result =await coffeeCollection.find().toArray();
       res.send(result);
@@ -49,7 +51,7 @@ async function run() {
       const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     })
-        // view the data
+        // view(findOne) the data
     app.get("/coffees/:id", async(req,res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)};
@@ -69,6 +71,19 @@ async function run() {
       };
       const result = await coffeeCollection.updateOne(filter, updateDoc, options);
       res.send(result);
+    })
+              //user related API
+            //get the data (ete data gulo 3000/users server e dekhabe)
+    app.get("/users", async(req,res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+              //post the data
+    app.post('/users', async(req,res)=>{
+        const userProfile = req.body;
+        console.log(userProfile);
+        const result = await usersCollection.insertOne(userProfile);
+        res.send(result);
     })
         // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
